355947602ff36901f2ac8405ea13b53302b884a8
Riska Kurnia Dewi <riska.kurnia81@ui.ac.id>
2020-03-30 14:56:12 +0700

    first commit

---
+++ src/main/java/com/apap/uts/uts_1806269801/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.apap.uts.uts_1806269801;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apap/uts/uts_1806269801/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apap.uts.uts_1806269801;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
